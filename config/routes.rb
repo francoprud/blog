f9a035677ece1c1c7b34c76d58d8701448eb9d69
Franco Prudhomme <franco.prudhomme@wolox.com.ar>
2015-01-19 11:24:10 -0300

    Add devise users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { registrations: "users/registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get 'static_pages/home'
   resources :articles do
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,10 +7,6 @@ describe UsersController do
   describe 'POST #create' do
 
     context 'When creating a user correctly' do
-
-      xit 'increments the users count' do
-        expect { post :create, user: { email: 'asdasdasd@gmail.com', password: 'password', password_confirmation: 'password' } }.to change{ User.count }.by(1)
-      end
     end
   end
 end
