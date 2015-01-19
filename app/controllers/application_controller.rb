ce59c67eb3f0dbaa715c232d0c72a569c6d73521
Franco Prudhomme <franco.prudhomme@wolox.com.ar>
2015-01-19 12:27:51 -0300

    Add fields for name while registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Includes Authorization mechanism
   include Pundit
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -14,5 +16,10 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     redirect_to(request.referrer || root_path)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
   end
 end
