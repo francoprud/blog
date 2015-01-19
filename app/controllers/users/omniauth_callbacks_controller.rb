b0ecc44fa6a2ba6836985b63e9cd7c7abe0bc698
Franco Prudhomme <franco.prudhomme@wolox.com.ar>
2015-01-19 12:22:55 -0300

    Remove unused controllers

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,29 +1,27 @@
-class Users
-  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.find_for_google_oauth2(request.env['omniauth.auth'], current_user)
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.find_for_google_oauth2(request.env['omniauth.auth'], current_user)
 
-      user_persistance('Google', 'devise.google_data')
-    end
+    user_persistance('Google', 'devise.google_data')
+  end
 
-    def facebook
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.find_for_facebook_oauth(request.env['omniauth.auth'], current_user)
+  def facebook
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.find_for_facebook_oauth(request.env['omniauth.auth'], current_user)
 
-      user_persistance('Facebook', 'devise.facebook_data')
-    end
+    user_persistance('Facebook', 'devise.facebook_data')
+  end
 
-    private
+  private
 
-    def user_persistance(kind, data)
-      if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
-        sign_in_and_redirect @user, event: :authentication
-      else
-        session[data] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
-      end
+  def user_persistance(kind, data)
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session[data] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
     end
   end
 end
